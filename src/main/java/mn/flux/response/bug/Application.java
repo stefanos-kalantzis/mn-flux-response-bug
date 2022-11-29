840e13bd0fc6f004b523ad6c08e4325bc412fd5b
Stefanos Kalantzis <stefanos.kalantzis@tx.group>
2022-11-29 13:31:28 +0100

    mn create-app mn-flux-response-bug --features cassandra,reactor --build maven

---
+++ src/main/java/mn/flux/response/bug/Application.java
@@ -0,0 +1,9 @@
+package mn.flux.response.bug;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
